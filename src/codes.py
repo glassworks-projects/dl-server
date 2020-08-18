a5898212066a42d18ba3065574d3e1fdf92f3209
glassworks-projects <ryan.michael.glassman@gmail.com>
2020-08-17 21:26:48 -0400

    working server

--- src/codes.py
+++ src/codes.py
@@ -1,55 +1,77 @@
 from random import SystemRandom
 from rstr import Rstr
 import sys
-from enum import Enum
 import json 
+import os
 
 INDENT = 4
+GENERATE = 'generate'
+NOT_FOUND = 'not found'
+USED = 'used'
+VALID = 'valid'
 
-class Args(Enum) :
-    GENERATE='generate'
+path = os.path.dirname(os.path.realpath(__file__))
 
-class Responses(Enum) :
-    NOT_FOUND = 'not found'
-    USED = 'used'
-    VALID = 'valid'
+codes_path = os.path.join(
+    path,
+    "obj/codes.json"
+)
 
 pattern = r'([A-Z]|[1-9]){3}-[A-Z]{3}'
 
-def generate(amt: int) :
+def generate(amt: int) -> None :
     rs = Rstr(SystemRandom())
-    dt = dict()
-    for _ in range(amt) :
-        dt[rs.xeger(pattern)] = True
-    
-    wrapper = {'codes' : dt}
-    with open('obj/codes.json', 'w') as fp : # change to append when ready
-        json.dump(wrapper, fp, indent=INDENT)
-    
+    codes = dict()
 
-def evaluate_code(code: str) -> str :
-    with open('/Users/ryanglassman/code/dl-server/src/obj/codes.json', 'r') as fp :
-        codes = json.load(fp)['codes']
-    
-    if code not in codes :
-        return Responses.NOT_FOUND.value
-    
-    if not codes[code] :
-        return Responses.USED.value
-    
-    # code is expended
-    codes[code] = False
-    
-    # overwrite file
-    with open('obj/codes.json', 'w') as fp :
+    if os.path.exists(codes_path) :
+        with open(codes_path, 'r') as fp :
+            codes = json.load(fp)['codes']
+
+        count = 0
+        new_codes = []
+
+        while count < amt :
+            cd = rs.xeger(pattern)
+            
+            if cd not in codes : 
+                new_codes.append(cd)
+                codes[cd] = True
+                count += 1
+        
+        # separate JSON of most recently generated codes
+        with open(os.path.join(path, 'obj/new-codes.json'), 'w') as fp :
+            json.dump({'codes' : {cd : True for cd in new_codes}}, fp, indent=INDENT)
+
+    else :
+        codes = {rs.xeger(pattern) : True for _ in range(amt)}
+
+    with open(codes_path, 'w') as fp :
         json.dump({'codes' : codes}, fp, indent=INDENT)
     
-    return Responses.VALID.value
+
+def evaluate_code(cd: str) -> str :
+    with open(codes_path, 'r') as fp :
+        codes = json.load(fp)['codes']
+    
+    if cd not in codes :
+        return NOT_FOUND
+    
+    if not codes[cd] :
+        return USED
+    
+    # code is expended
+    codes[cd] = False
+    
+    # overwrite file
+    with open(codes_path, 'w') as fp :
+        json.dump({'codes' : codes}, fp, indent=INDENT)
+    
+    return VALID
     
 
 if __name__ == '__main__' :
 
-    if len(sys.argv) > 2 and sys.argv[1] == Args.GENERATE.value :
+    if len(sys.argv) > 2 and sys.argv[1] == GENERATE :
         try :
             code_count = int(sys.argv[2])
             print("generating...")
@@ -59,5 +81,4 @@ if __name__ == '__main__' :
 
     elif len(sys.argv) == 2 :
         code = sys.argv[1]
-        # print("evaluating code {}: ".format(code))
         print(evaluate_code(code))
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
-import cors from 'cors';
 import { PythonShell } from 'python-shell';
 
 const NOT_FOUND = 'not found';
 const USED = 'used';
 const VALID = 'valid';
 
-const app = express();
 const bodyParser = require('body-parser');
+const app = express();
 
-app.use(cors());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -34,34 +32,39 @@ app.post("/python-test", (req, res) => {
 	if (code) {
 		options.args = [code]
 		const pyshell = new PythonShell('codes.py', options);
-		let data;
 
 		pyshell.on('message', function (message) {
 			// received a message sent from the Python script (a simple "print" statement)
 			switch(message) {
 				case NOT_FOUND: {
-
+					res.sendStatus(404);
 					break;
 				}
 
 				case USED: {
-
+					res.status(403).send('Code already used');
 					break;
 				}
 
 				case VALID: {
-
+					res.sendFile(process.env.ASSET_PATH);
 					break;
 				}
 
-				default: break;
+				default: {
+					res.status(500).send(
+						'Encountered an unexpected response when validating download code'
+					);
+					break;
+				}
 			};
 		});
 		
 		// end the input stream and allow the process to exit
 		pyshell.end(function (err) {
 			if (err) throw err;
-			res.send(data);
 		});
-	}; // otherwise do something else
+	} else {
+		res.status(400).send("No download code provided");
+	}
 })
